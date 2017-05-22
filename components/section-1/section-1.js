f12a06ecf9d94a278590f8e5333bb934fd6bcb1a
Karen Yimeng Wang <kywang@Karens-MacBook-Pro-4.local>
2017-05-21 23:53:30 -0700

    added basic interaction

--- components/section-1/section-1.js
+++ components/section-1/section-1.js
@@ -1,17 +1,66 @@
 "use strict";
 
-window.addEventListener("load", function() {
-  var button1 = document.getElementById("button-1");
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
 
-  var button1WasClicked = false;
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+      var textEl = event.target.querySelector('p');
 
-  button1.addEventListener("click", function() {
-    button1WasClicked = !button1WasClicked
-    if (button1WasClicked) {
-      button1.setAttribute("class", "btn btn-warning");
-    } else {
-      button1.setAttribute("class", "btn btn-danger");
+      textEl && (textEl.textContent =
+        'moved a distance of '
+        + (Math.sqrt(event.dx * event.dx +
+                     event.dy * event.dy)|0) + 'px');
     }
   });
-});
 
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
+
+  var activities = {
+    "hiking": {
+      "value": 1,
+      "duration": 4
+    },
+    "date": {
+      "value": 4,
+      "duration": 3
+    },
+    "gym": {
+      "value": 1,
+      "duration": 1
+    },
+    "beach": {
+      "value": 5,
+      "duration": 7
+    }
+  }
