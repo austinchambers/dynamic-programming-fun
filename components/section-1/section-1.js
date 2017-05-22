5a3bea58174603b0d799fc06e40c18e9ecb2dbf4
Austin Chambers <austin@intapp.com>
2017-05-22 04:19:13 -0700

    #3 - Indicate Dropzone when drag and drop is taking place.

--- components/section-1/section-1.js
+++ components/section-1/section-1.js
@@ -64,3 +64,54 @@ interact('.draggable')
       "duration": 5
     }
   }
+
+
+interact('.draggable')  
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+  });
+
+// enable draggables to be dropped into this
+interact('.dropzone').dropzone({  
+  // Require a 50% element overlap for a drop to be possible
+  overlap: 0.50,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add('drop-active');
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget,
+        dropzoneElement = event.target;
+
+    event.relatedTarget.classList.add('notdropped');
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target');
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target');
+  },
+  ondrop: function (event) {
+    event.relatedTarget.classList.remove('notdropped');
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active');
+    event.target.classList.remove('drop-target');
+  }
+});
