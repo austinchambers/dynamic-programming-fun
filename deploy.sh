25034db147a0a2fe52edd79b35752c0c3a507dbc
Austin Chambers <austin@intapp.com>
2017-05-23 04:49:56 -0700

    Fixed issue with deploy script (no .src directory)

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 echo "Syncing the contents with S3"
-echo "Syncing the contents with S3"
-aws s3 sync --region "us-west-2" --exclude ".DS_Store" ./src s3://cs247team3
+aws s3 sync --region "us-west-2" --exclude ".DS_Store" . s3://cs247team3
