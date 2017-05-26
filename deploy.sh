6b1d0d9066e83585d0ce187c82a1f8846038afcb
austin <austin1@stanford.edu>
2017-05-25 22:58:33 -0700

    Excluding *.git folder from s3 deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 echo "Syncing the contents with S3"
-aws s3 sync --region "us-west-2" --exclude ".DS_Store" . s3://dp.dignata.net
+aws s3 sync --region "us-west-2" --exclude ".DS_Store"  --exclude ".git" . s3://dp.dignata.net
